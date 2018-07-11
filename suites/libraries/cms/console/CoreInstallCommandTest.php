0a9950aad1f60f5d3b521c147e5450354f7f2946
Bosun Egberinde <bosunski@gmail.com>
2018-07-11 17:26:52 +0100

    Added more tests

--- suites/libraries/cms/console/CoreInstallCommandTest.php
+++ suites/libraries/cms/console/CoreInstallCommandTest.php
@@ -41,6 +41,7 @@ class CoreInstallCommandTest extends \PHPUnit\Framework\TestCase
 	protected function setUp()
 	{
 		$this->object = new CoreInstallCommand;
+		JLoader::registerNamespace('Joomla\\CMS\\Installation', JPATH_INSTALLATION . '/src', false, false, 'psr4');
 	}
 
 	/**
@@ -98,5 +99,62 @@ class CoreInstallCommandTest extends \PHPUnit\Framework\TestCase
 		$actualKeys = array_keys($this->object->getOptionsTemplate());
 
 		$this->assertEquals($expectedKeys, $actualKeys);
+	}
+
+	/**
+	 * Tests the parseIniFile method
+	 *
+	 * @return void
+	 *
+	 * @since 4.0
+	 */
+	public function testParseIniFile()
+	{
+		$content = "name=Joomla\nyear=2018";
+		touch('config.ini');
+		file_put_contents('config.ini', $content);
+
+		$options = $this->object->parseIniFile('config.ini');
+
+		self::assertEquals('Joomla', $options['name']);
+		self::assertEquals('2018', $options['year']);
+
+		\Joomla\Filesystem\File::delete('config.ini');
+	}
+
+	/**
+	 * Tests the processUninteractiveInstallation method
+	 *
+	 * @return void
+	 *
+	 * @since 4.0
+	 */
+	public function testProcessUninteractiveInstallation()
+	{
+		$iniContent = "name=Joomla\nyear=2018";
+		$jsonContent = '{"name":"Joomla", "year":"2018"}';
+
+		touch('config.bat');
+		touch('config.ini');
+
+		$options = $this->object->processUninteractiveInstallation('config.joomla');
+		self::assertEquals('Unable to locate file specified', $options);
+
+		$options = $this->object->processUninteractiveInstallation('config.bat');
+		self::assertEquals('The file type specified is not supported', $options);
+
+		file_put_contents('config.ini', $iniContent);
+		$options = $this->object->processUninteractiveInstallation('config.ini', false);
+		self::assertEquals('Joomla', $options['name']);
+		self::assertEquals('2018', $options['year']);
+
+		file_put_contents('config.json', $jsonContent);
+		$options = $this->object->processUninteractiveInstallation('config.json', false);
+		self::assertEquals('Joomla', $options['name']);
+		self::assertEquals('2018', $options['year']);
+
+		\Joomla\Filesystem\File::delete('config.ini');
+		\Joomla\Filesystem\File::delete('config.json');
+		\Joomla\Filesystem\File::delete('config.bat');
 	}
 }
